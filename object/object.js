80a13d3513a3ab72201a7706a7d3e1911b3ece38
cupyong <405329943@qq.com>
2016-03-18 10:44:59 +0800

    es6 object

---
+++ object/object.js
@@ -0,0 +1,11 @@
+/**
+ * Created by oopsdata1 on 16-3-18.
+ */
+function Person(name, age, job) {
+    this.name = name;
+    this.age = age;
+    this.job = job;
+}
+
+var person1 = new Person('Weiwei', 27, 'Student');
+var person2 = new Person('Lily', 25, 'Doctor');
